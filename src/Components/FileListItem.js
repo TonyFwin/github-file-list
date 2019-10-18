7ae45cde219332079b1c46818302ad2248c2073f
Tony Nguyen <tony.nguyen@epicosity.com>
2019-10-18 16:05:16 -0500

    Create time component

--- src/Components/FileListItem.js
+++ src/Components/FileListItem.js
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types';
 
 import FileName from './FileName';
 import CommitMessage from './CommitMessage';
+import Time from './Time';
 
 const FileListItem = ({ file }) => (
   <tr className="file-list-item" key={file.id}>
     <td className="file-name-wrapper">
       <FileName file={file} />
       <CommitMessage commit={file.latestCommit} />
+      <div className="age">
+        <Time time={file.updated_at}/>
+      </div>
     </td>
   </tr>
 )
